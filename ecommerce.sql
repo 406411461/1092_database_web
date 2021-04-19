19d909009e1016b75406af46863a6a2d0a3313c1
Wu <406411461@gms.tku.edu.tw>
2021-04-19 21:39:30 +0800

    0419/shop product

---
+++ ecommerce.sql
@@ -0,0 +1,188 @@
+-- MySQL dump 10.13  Distrib 8.0.22, for Win64 (x86_64)
+--
+-- Host: 127.0.0.1    Database: ecommerce
+-- ------------------------------------------------------
+-- Server version	8.0.22
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!50503 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `cart`
+--
+
+DROP TABLE IF EXISTS `cart`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `cart` (
+  `pid` int NOT NULL,
+  `pname` varchar(45) DEFAULT NULL,
+  `cost` int DEFAULT NULL,
+  PRIMARY KEY (`pid`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `cart`
+--
+
+LOCK TABLES `cart` WRITE;
+/*!40000 ALTER TABLE `cart` DISABLE KEYS */;
+INSERT INTO `cart` VALUES (1,'vans style_36(Red)',2780),(3,'vans style_36(Blue)',2780),(4,'NIKE AIR ZOOM TYPE N354',4980);
+/*!40000 ALTER TABLE `cart` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `category`
+--
+
+DROP TABLE IF EXISTS `category`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `category` (
+  `cId` int NOT NULL,
+  `cname` varchar(45) DEFAULT NULL,
+  PRIMARY KEY (`cId`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `category`
+--
+
+LOCK TABLES `category` WRITE;
+/*!40000 ALTER TABLE `category` DISABLE KEYS */;
+INSERT INTO `category` VALUES (1,'nike'),(2,'vans'),(3,'new balence'),(4,'adidas'),(5,'fila');
+/*!40000 ALTER TABLE `category` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `member`
+--
+
+DROP TABLE IF EXISTS `member`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `member` (
+  `mId` int NOT NULL,
+  `name` varchar(45) DEFAULT NULL,
+  `email` varchar(45) DEFAULT NULL,
+  `address` varchar(45) DEFAULT NULL,
+  `phone` varchar(45) DEFAULT NULL,
+  PRIMARY KEY (`mId`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `member`
+--
+
+LOCK TABLES `member` WRITE;
+/*!40000 ALTER TABLE `member` DISABLE KEYS */;
+INSERT INTO `member` VALUES (22,'Kevin','Kevin777@gmail.com',NULL,'0905136578'),(43,'Posey','Posey596@gmail.com',NULL,'0965484123'),(69,'John','John0909@gmail.com',NULL,'0909568788'),(71,'Howard','Howard0613@gmail.com',NULL,'0932164587'),(666,'Lisa','lisa666@gmail.com',NULL,'0965485213');
+/*!40000 ALTER TABLE `member` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `order`
+--
+
+DROP TABLE IF EXISTS `order`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `order` (
+  `tid` int NOT NULL,
+  `pId` int DEFAULT NULL,
+  `mId` int DEFAULT NULL,
+  `cost` int DEFAULT NULL,
+  `payment` varchar(45) DEFAULT NULL,
+  `delievery` varchar(45) DEFAULT NULL,
+  `time` varchar(45) DEFAULT NULL,
+  PRIMARY KEY (`tid`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `order`
+--
+
+LOCK TABLES `order` WRITE;
+/*!40000 ALTER TABLE `order` DISABLE KEYS */;
+INSERT INTO `order` VALUES (259,3,43,2000,'貨到付款','超商',NULL);
+/*!40000 ALTER TABLE `order` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `product`
+--
+
+DROP TABLE IF EXISTS `product`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `product` (
+  `pId` int NOT NULL,
+  `pname` varchar(45) DEFAULT NULL,
+  `price` int DEFAULT NULL,
+  `image` varchar(45) DEFAULT NULL,
+  `profile` varchar(45) DEFAULT NULL,
+  `cId` int DEFAULT NULL,
+  PRIMARY KEY (`pId`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `product`
+--
+
+LOCK TABLES `product` WRITE;
+/*!40000 ALTER TABLE `product` DISABLE KEYS */;
+INSERT INTO `product` VALUES (1,'vans style_36(Red)',2780,NULL,'版型正常',2),(2,'new balance 996',3980,NULL,'版型正常',3),(3,'vans style_36(Blue)',2780,NULL,'版型偏大，建議小半號，腳板窄建議小一號',2),(4,'NIKE AIR ZOOM TYPE N354',4980,NULL,'版型正常',1);
+/*!40000 ALTER TABLE `product` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `product_review`
+--
+
+DROP TABLE IF EXISTS `product_review`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `product_review` (
+  `pId` int DEFAULT NULL,
+  `mId` int DEFAULT NULL,
+  `pname` varchar(45) DEFAULT NULL,
+  `comment` varchar(45) DEFAULT NULL,
+  `time` varchar(45) DEFAULT NULL,
+  KEY `fk1_idx` (`pId`),
+  CONSTRAINT `fk1` FOREIGN KEY (`pId`) REFERENCES `product` (`pId`) ON DELETE SET NULL ON UPDATE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `product_review`
+--
+
+LOCK TABLES `product_review` WRITE;
+/*!40000 ALTER TABLE `product_review` DISABLE KEYS */;
+INSERT INTO `product_review` VALUES (1,43,'vans style_36(Red)','good','w'),(1,69,'vans style_36(Red)','price isn`t expensive','a'),(3,22,'vans style_36(Blue)','good to dress','s'),(NULL,71,'vans AUTHENTIC TRUE WHITE','doesn`t meet my except','d'),(NULL,96,'vans AUTHENTIC TRUE WHITE','not bad','f');
+/*!40000 ALTER TABLE `product_review` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2021-04-19 21:38:30
