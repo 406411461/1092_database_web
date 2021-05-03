9a18007c89236a399564e518b90ded44fcaf672d
Wu <406411461@gms.tku.edu.tw>
2021-05-03 19:45:26 +0800

    0503-2/ CRUD

--- routes/cart.js
+++ routes/cart.js
@@ -9,7 +9,7 @@ router.get('/', cartController.getCart);
 router.post('/create', cartController.createCart);
 router.post('/update', cartController.updateCart);
 router.get('/delete', cartController.deleteCart);
-router.get('/delete/:pId', cartController.delete2Cart);
+//router.get('/delete/:pId', cartController.delete2Cart);
 
 
 
